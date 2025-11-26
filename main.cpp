b118b5f3feda3fea64584151da3383b7263e8931
Ruheluobixieqing <2244216991@qq.com>
2025-11-26 16:27:34 +0800

    2025-11-26

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll long long
+const int N = 2e5+4;
+int a[N];
+
 int main()
 {
-    double n,t;
-    std::cin>>n>>t;
-    double base = 4000;
-    double ans = 0;
-    if(n>=6)
+    std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+    int n;
+    std::cin >> n;
+    std::vector<int> v(n+1), prefix(n+1), prefix_xor(n+1);
+    for (int i = 1; i <=n; i ++)
     {
-        if(t>=3.5) ans = std::max(ans,(n-2)/2*200*20*1.4);
-        else if(t>=3) ans = std::max(ans,(n-2)/2*200*20*1.2);
-        else if(t>=2.5) ans = std::max(ans,(n-2)/2*200*20.0);
+        std::cin >> v[i];
+        prefix[i] = prefix[i-1] + v[i];
+        prefix_xor[i] = prefix_xor[i-1] ^ v[i];
     }
-    if(n>=4)
+
+    ll ans = 0;
+    for (int i = 1, j = 1; i <= n && j <= n; )
     {
-        if(t>=3) ans = std::max(ans,1100.0);
-        else if(t>=2.5) ans = std::max(ans,900.0);
-        else if(t>=2) ans = std::max(ans,700.0);
+        if ((prefix_xor[j] ^ prefix_xor[i - 1]) == (prefix[j] - prefix[i - 1]))
+        {
+            ans += j - i + 1;
+            j ++;
+        }
+        else i++;
     }
-    if(n>=3)
-    {
-        if(t>=3) ans = std::max(ans,500.0);
-        else if(t>=2.5) ans = std::max(ans,400.0);
-        else if(t>=2) ans = std::max(ans,300.0);
-    }
-    if(n>=2&&t>=2.5) ans = std::max(ans,200.0);
-    std::cout<<std::fixed<<std::setprecision(2)<<ans+base<<'\n';
+
+    std::cout << ans << '\n';
     return 0;
 }
