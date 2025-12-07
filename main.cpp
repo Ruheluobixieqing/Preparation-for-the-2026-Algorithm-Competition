144b784d56891533a46356422b44820e0e90b01d
Ruheluobixieqing <2244216991@qq.com>
2025-12-07 21:57:19 +0800

    2025-12-7

--- main.cpp
+++ main.cpp
@@ -1,34 +1,17 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<long long> a(n), b(n);
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    for (int i = 0; i < n; ++i) cin >> b[i];
-
-    long long delta = 0;
-    int sign = 1;               // 当前真实值 = sign * x + delta，x 初值为 0
-    for (int i = 0; i < n; ++i) {
-        long long red  = delta - sign * a[i];
-        long long blue = b[i] - delta;
-        if (red > blue) {       // 选红更优
-            delta = red;
-        } else {                // 选蓝更优
-            delta = blue;
-            sign = -sign;       // 翻转符号
-        }
+std::string int_to_binary_string(int num, int n) {
+    std::string result;
+    for (int i = n - 1; i >= 0; --i) {
+        result += (num & (1 << i)) ? '1' : '0';
     }
-    cout << delta << '\n';
+    return result;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--) solve();
+    int num = 156;
+    int n = 8;
+    std::string binary = int_to_binary_string(num, n);
+    std::cout << binary << std::endl;  // 输出：00000101
     return 0;
 }
